2ec342622433cf8f11bfa88b94a11762c09b761f
Noabsa <noelia.abalo@gmail.com>
2026-08-16 16:22:02 +0200

    Add some guides

--- apps/web/src/infrastructure/repositories/JsonGuideRepository.ts
+++ apps/web/src/infrastructure/repositories/JsonGuideRepository.ts
@@ -1,12 +1,16 @@
 import { Guide, type IGuide } from '../../domain/entities/Guide';
 import type { GuideRepository } from '../../domain/repositories/GuideRepository';
+import almodovarDelRio from '../data/guides/almodovar-del-rio.json';
 import mezquitaCordoba from '../data/guides/mezquita-cordoba.json';
+import ochavilloDelRio from '../data/guides/ochavillo-del-rio.json';
 import puenteRomano from '../data/guides/puente-romano.json';
 import puenteSanRafael from '../data/guides/puente-san-rafael.json';
 import tardeCordoba from '../data/guides/tarde-cordoba.json';
 
 const guides: Record<string, IGuide> = {
+  'almodovar-del-rio': almodovarDelRio as unknown as IGuide,
   'mezquita-cordoba': mezquitaCordoba as unknown as IGuide,
+  'ochavillo-del-rio': ochavilloDelRio as unknown as IGuide,
   'puente-romano': puenteRomano as unknown as IGuide,
   'puente-san-rafael': puenteSanRafael as unknown as IGuide,
   'tarde-cordoba': tardeCordoba as unknown as IGuide,
